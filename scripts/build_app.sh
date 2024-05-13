9747dbc92bfb6632c4192b4888ace08c769646b6
Linux <you@example.com~>
2024-05-14 00:37:01 +0300

    Make script build.app

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-
 # Проверка наличия параметров
 if [[ $# -lt 2 ]]; then
-    echo "Need 2 params"
-    exit 2
+    echo "Input two parameters"
+    exit 1
 fi
 
 # Определение имени входных файлов
 size=$1
 sort=$2
 
-
-for file in *.c; do
-    echo "$file"
-    base=$(basename -s .c "$file")
-    gcc -std=c99 -Wall -Wvla -Werror -c "$file" -o "apps/$base".o
-done
-
-gcc -o apps/app_"$size"_"$sort".exe apps/*.o -lm -DSIZE="$size" -DSORT="$sort"
-./scripts/clean.sh 
+# Компиляция
+gcc -std=c99 -Wall -Wvla -Werror -Wpedantic -Wextra -Wfloat-conversion -Wfloat-equal ./*.c -o apps/app_"$size"_"$sort".exe -DSIZE="$size" -DSORT="$sort"
--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,6 +4,7 @@ path_to_script=$(dirname "$(readlink -f "$0")")
 path="$path_to_script"/../apps
 
 rm -f "$path"/*.o
+rm -f "$path"/*.exe
 rm -f "$path"/*temp.txt*
 
 exit 0
