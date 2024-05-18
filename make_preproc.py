3fd2f6e5edc07df17f6bf416d12709695b5b2066
Linux <you@example.com~>
2024-05-18 19:16:19 +0300

    make_preproc.py

--- make_preproc.py
+++ make_preproc.py
@@ -4,12 +4,6 @@ import statistics
 """подготовка данных из набора, провести первичный анализ: посчитать среднее арифметическое, медианное,
 найти максимум и минимум, вычислить нижний и верхний квартили, etc."""
 
-path_to_files = os.path.dirname(os.path.abspath(__file__)) + "/data/"
-files = os.listdir(path_to_files)
-
-if not (os.path.exists(f"{path_to_files}/sort_data/")):
-    os.mkdir(f"{path_to_files}/sort_data/")
-
 
 def strcspn(string, characters):
     """
@@ -25,11 +19,13 @@ def strcspn(string, characters):
 
 
 def extract_file_size(input_string):
+    # Получение длины массива из названия файла
     number = "".join(filter(str.isdigit, input_string))
     return int(number[:-1])
 
 
 def calc_rse(time_array, count):
+    # Подсчет RSE
     t_avg = sum(time_array) / count
     dispersion = 0
     for i in range(count):
@@ -42,21 +38,12 @@ def calc_rse(time_array, count):
     return rse
 
 
-def check_is_data_unique(f, data: str):
-    while True:
-        line = f.readline()
-        if not line:
-            break
-        if line == data:
-            return False
-    return True
-
-
 def collect_data(file):
+    """Функция для сбора данных из файла"""
     count = 0
     data = []
-    if not (os.path.exists(f"{path_to_files}/{file}")):
-        return None, None
+    if not (os.path.exists(file)):
+        return data, count
     with open(file, mode="r") as f:
         while True:
             line = f.readline().strip()
@@ -68,9 +55,30 @@ def collect_data(file):
     return data, count
 
 
+def make_file_unique(filename):
+    # Удаление дубликатов из файла
+    with open(filename, "r") as file:
+        lines = file.readlines()
+
+    unique_lines = set(lines)
+
+    with open(filename, "w") as file:
+        for line in unique_lines:
+            file.write(line)
+
+
+# Получение путя к файлам и названий файлов
+path_to_files = os.path.dirname(os.path.abspath(__file__)) + "/data/"
+files = os.listdir(path_to_files)
+
+if not (os.path.exists(f"{path_to_files}/sort_data/")):
+    os.mkdir(f"{path_to_files}/sort_data/")
+
+# Перебираем все файлы в папке data
 for file in files:
     if not file.endswith(".txt"):
         continue
+
     size = extract_file_size(file)
     experiment = strcspn(file, "_")
     stats_filename = file[:experiment] + "_" + file[experiment + 2 + len(str(size)) :]
@@ -91,14 +99,11 @@ for file in files:
         q1 = statistics.median(data[: (count // 2)])
         q3 = statistics.median(data[(count // 2) + 1 :])
 
+    # Запись статистики в файл
     output_file = path_to_files + "sort_data/" + stats_filename
-    cur_data, count = collect_data(output_file)
-    string_to_output = f"{size} {average} {median} {min_value} {max_value} {q1} {q3}"
-    if cur_data and string_to_output not in cur_data:
-        with open(output_file, "a") as f:
-            f.read(string_to_output)
+    string_to_output = f"{size} {average} {median} {min_value} {max_value} {q1} {q3}\n"
 
-    # Для таблицы
-    # rse = calc_rse(data, count)
-    # print(average, median, min_value, max_value, q1, q3, rse)
-    break
+    with open(output_file, "a+") as f:
+        f.write(string_to_output)  # Добавляем статистику в конец файла
+
+    make_file_unique(output_file)
