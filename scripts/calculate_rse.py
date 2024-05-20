dd3e4c9bcde8052cb7ef8028079dd18961585cab
Linux <you@example.com~>
2024-05-21 02:41:48 +0300

    little fix

--- scripts/calculate_rse.py
+++ scripts/calculate_rse.py
@@ -14,9 +14,9 @@ def check_rse(time_array, count):
     for i in range(count):
         dispersion += (time_array[i] - t_avg) ** 2
 
-    dispersion /= (count - 1)
-    standard_deviation = dispersion ** 0.5
-    std_error = standard_deviation / count ** 0.5
+    dispersion /= count - 1
+    standard_deviation = dispersion**0.5
+    std_error = standard_deviation / count**0.5
     rse = std_error * 100 / t_avg
     if rse < 1:
         return True
@@ -26,7 +26,7 @@ def check_rse(time_array, count):
 
 times = []
 path_to_program = (sys.argv)[1]
-with open(path_to_program, 'r') as f:
+with open(path_to_program, "r") as f:
     while True:
         line = f.readline()
         if not line:
